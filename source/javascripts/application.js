63b0c7a1bec066af10b868719a1a9e7ce5df3af3
Polarblau <polarblau@gmail.com>
2013-10-04 18:02:07 +0300

    The new site, version 1.

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -7,8 +7,22 @@ $(function(){
 
   function allComplete() {
     $('body').addClass('reveal');
+    setTimeout(flicker, 2000);
   }
 
-  $('h1 img, img.background').preload(onSuccess, onError, allComplete);
+  $('.background img').preload(onSuccess, onError, allComplete);
+
+  $('body').on('dblclick', function() {
+    $(this).toggleClass('grid');
+  });
+
+  var $background = $('#header .background, #footer .background');
+  function flicker() {
+    var opacity = Math.min(1, Math.random() + 0.6),
+        duration = Math.random() * 500 + 250;
+
+    $background.animate({ opacity: opacity }, duration, flicker);
+  }
+
 
 });
