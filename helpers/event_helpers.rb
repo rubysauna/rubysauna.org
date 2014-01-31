47051042cf65e5517b0bdc02791fe54366ecdd93
Polarblau <polarblau@gmail.com>
2014-01-31 16:40:12 +0200

    Moving speakers into partial and data.

--- helpers/event_helpers.rb
+++ helpers/event_helpers.rb
@@ -8,6 +8,14 @@ module EventHelpers
     sorted_events.reject {|event| upcoming_event?(event) }
   end
 
+  def upcoming_events
+    sorted_events.select {|event| upcoming_event?(event) }
+  end
+
+  def next_event
+    upcoming_events.first
+  end
+
   def event_path(event)
     "/archive/#{event.date.year}/#{event.date.month}/#{event.date.day}/index.html"
   end
